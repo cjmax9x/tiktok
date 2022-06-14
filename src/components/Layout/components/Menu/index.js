a5daedd2d25812d7fdeb1c3e19abe3cb3b3f7f89
Tung Bui <cjmax9x@gmail.com>
2022-06-14 08:38:18 +0700

    css header #5

--- src/components/Layout/components/Menu/index.js
+++ src/components/Layout/components/Menu/index.js
@@ -3,22 +3,48 @@ import classNames from 'classnames/bind';
 import styles from './Menu.module.scss';
 import { Wrapper as ProperWrapper } from '../Proper';
 import MenuItem from './MenuItem';
+import Header from './Header';
+import { useState } from 'react';
 
 const cx = classNames.bind(styles);
 
 function Menu({ children, items = [] }) {
+    const [history, setHistory] = useState([{ data: items }]);
+    const current = history[history.length - 1];
     const renderItem = () => {
-        return items.map((item, index) => <MenuItem key={index} data={item} />);
+        return current.data.map((item, index) => {
+            const isParent = !!item.children;
+            return (
+                <MenuItem
+                    key={index}
+                    data={item}
+                    onClick={() => {
+                        if (isParent) {
+                            setHistory((prev) => [...prev, item.children]);
+                        }
+                    }}
+                />
+            );
+        });
     };
     return (
         <Tippy
-            // delay={[0, 700]}
-            visible
+            delay={[0, 700]}
             placement="bottom"
             interactive
             render={(attrs) => (
                 <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
-                    <ProperWrapper custom={cx('menu-proper')}>{renderItem()}</ProperWrapper>
+                    <ProperWrapper custom={cx('menu-proper')}>
+                        {history.length > 1 && (
+                            <Header
+                                onBack={() => {
+                                    setHistory((prev) => prev.slice(0, prev.length - 1));
+                                }}
+                                title="Language"
+                            />
+                        )}
+                        {renderItem()}
+                    </ProperWrapper>
                 </div>
             )}
         >
